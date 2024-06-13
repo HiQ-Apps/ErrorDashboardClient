f198ed4cc281b3afc3499c3de4a90bcd0e5ee449
Daywick <derekli11204@gmail.com>
2024-06-13 04:30:46 -0400

    dry namespace navbar

--- client/src/pages/Namespace/NamespaceDetail.tsx
+++ client/src/pages/Namespace/NamespaceDetail.tsx
@@ -1,11 +1,9 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import { NamespaceSidebar, ErrorDataTable } from "components/composite";
-import { BaseButton } from "components/base";
 import { useGetNamespaceByIdQuery } from "features/namespaceApiSlice";
 
 const NamespaceDetail = () => {
-  const navigate = useNavigate();
   const { id } = useParams();
 
   if (!id) {
@@ -14,59 +12,6 @@ const NamespaceDetail = () => {
 
   const { data: namespace, isLoading } = useGetNamespaceByIdQuery(id);
 
-  const handleNamespaceLogsClick = () => {
-    navigate(`/namespace/${id}/logs`);
-  };
-
-  const handleNamespaceDetailsClick = () => {
-    navigate(`/namespace/${id}`);
-  };
-
-  const handleNamespaceSettingClick = () => {
-    navigate(`/namespace/${id}/settings`);
-  };
-
-  const handleNamespaceConsoleClick = () => {
-    navigate(`/namespace/console`);
-  };
-
-  const handleNamespaceMetricClick = () => {
-    navigate(`/namespace/${id}/metrics`);
-  };
-
-  const links = [
-    <BaseButton
-      content="Console"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceConsoleClick}
-    />,
-    <BaseButton
-      content="Details"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceDetailsClick}
-    />,
-    <BaseButton
-      content="Logs"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceLogsClick}
-    />,
-    <BaseButton
-      content="Settings"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceSettingClick}
-    />,
-    <BaseButton
-      content="Metrics"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceMetricClick}
-    />,
-  ];
-
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -74,7 +19,7 @@ const NamespaceDetail = () => {
   return (
     <div className="bg-slate-50 text-slate-900 min-h-screen w-full flex flex-row relative dark:bg-slate-800 dark:text-slate-200">
       <div className="bg-slate-50 w-52 p-4 bg-gray-200 h-screen dark:bg-slate-800">
-        <NamespaceSidebar links={links} />
+        <NamespaceSidebar />
       </div>
       <div className="flex-1 p-4">
         <h1>Service Name: {namespace?.service_name}</h1>
--- client/src/pages/Namespace/NamespaceLogs.tsx
+++ client/src/pages/Namespace/NamespaceLogs.tsx
@@ -1,6 +1,5 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-import { BaseButton } from "components/base";
 import { NamespaceSidebar, ErrorLogTable } from "components/composite";
 
 const NamespaceLogs = () => {
@@ -10,65 +9,10 @@ const NamespaceLogs = () => {
     throw new Error("Namespace ID is required");
   }
 
-  const navigate = useNavigate();
-
-  const handleNamespaceConsoleClick = () => {
-    navigate(`/namespace/console`);
-  };
-
-  const handleNamespaceDetailsClick = () => {
-    navigate(`/namespace/${id}`);
-  };
-
-  const handleNamespaceSettingsClick = () => {
-    navigate(`/namespace/${id}/settings`);
-  };
-
-  const handleNamespaceLogsClick = () => {
-    navigate(`/namespace/${id}/logs`);
-  };
-
-  const handleNamespaceMetricClick = () => {
-    navigate(`/namespace/${id}/metrics`);
-  };
-
-  const links = [
-    <BaseButton
-      content="Console"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceConsoleClick}
-    />,
-    <BaseButton
-      content="Details"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceDetailsClick}
-    />,
-    <BaseButton
-      content="Logs"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceLogsClick}
-    />,
-    <BaseButton
-      content="Settings"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceSettingsClick}
-    />,
-    <BaseButton
-      content="Metrics"
-      size="sm"
-      variant="sidenavbutton"
-      onClick={handleNamespaceMetricClick}
-    />,
-  ];
-
   return (
     <div className="bg-slate-50 text-slate-900 min-h-screen w-full flex flex-row relative dark:bg-slate-800 dark:text-slate-200">
       <div className="bg-slate-50 w-52 p-4 bg-gray-200 h-screen dark:bg-slate-800">
-        <NamespaceSidebar links={links} />
+        <NamespaceSidebar />
       </div>
       <div>
         <p>Logs for the namespace errors</p>
