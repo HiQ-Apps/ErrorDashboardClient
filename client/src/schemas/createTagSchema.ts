50646b0c2447d21d47ff6beb5a34fc0275c899ae
Daywick <derekli11204@gmail.com>
2024-07-01 05:53:15 -0400

    Added coloring to tags

--- client/src/schemas/createTagSchema.ts
+++ client/src/schemas/createTagSchema.ts
@@ -4,6 +4,7 @@ export const createTagSchema = z.object({
   tag_key: z.string().max(20),
   tag_value: z.string().max(20),
   error_id: z.string().uuid(),
+  tag_color: z.string().max(7),
 });
 
 export type CreateTagSchema = z.infer<typeof createTagSchema>;
