815270f5453eaf38adcaf90afa68ee1e6956f827
melissah717 <melissa.h717@outlook.com>
2024-08-12 13:20:35 -0700

    Release notes page

---
+++ client/src/components/composite/ChangeLogCard/ChangeLogCard.tsx
@@ -0,0 +1,32 @@
+import { Card, CardHeader, CardTitle, CardContent } from "components/ui/card";
+import React from "react";
+
+interface ChangeLogCardProps {
+  version: string;
+  date: string;
+  changes: string[];
+}
+
+const ChangeLogCard: React.FC<ChangeLogCardProps> = ({
+  version,
+  date,
+  changes,
+}) => {
+  return (
+    <Card className="flex flex-col md:flex-row border-none shadow-none p-3 min-h-[400px]">
+      <div className="flex flex-col w-1/4">
+        <CardTitle className="text-2xl font-semibold mb-4">{version}</CardTitle>
+        <CardHeader className="w-full place-items-center mb-4 p-0 flex flex-row text-nowrap">
+          <span className="ml-0 text-lg text-default">{date}</span>
+        </CardHeader>
+      </div>
+      <CardContent className="list-disc space-y-2 md:pl-10 w-full">
+        {changes.map((change, index) => (
+          <li key={index}>{change}</li>
+        ))}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ChangeLogCard;
--- client/src/pages/Base/Releases.tsx
+++ client/src/pages/Base/Releases.tsx
@@ -1,10 +1,33 @@
-import { Card } from "components/ui/card";
-
-import { HomeSidebar } from "components/composite";
+import { ChangeLogCard, HomeSidebar } from "components/composite";
 import { usePageDimensions } from "hooks/usePageDimensions";
 
 const Releases = () => {
   const { height } = usePageDimensions();
+  const releaseData = [
+    {
+      version: "HiGuard 1.0.1",
+      date: "August 8th, 2024",
+      changes: [
+        "Base backend services for namespace, users, errors were implemented!",
+        "Base frontend skeleton complete for main services.",
+        "Client SDK initialized. SKRRRRRRRRRR. Lorem Ipsum Testing to see the bullet points to see if they wrap correclty so I'm writing a quite mundane paragraph to act as a placeholder to test a big wall of text in a row., Testing to see the bullet points to see if they wrap correclty so I'm writing a quite mundane paragraph to act as a placeholder to test a big wall of text in a row., Testing to see the bullet points to see if they wrap correclty so I'm writing a quite mundane paragraph to act as a placeholder to test a big wall of text in a row., Testing to see the bullet points to see if they wrap correclty so I'm writing a quite mundane paragraph to act as a placeholder to test a big wall of text in a row.",
+        "Testing to see the bullet points to see if they wrap correclty so I'm writing a quite mundane paragraph to act as a placeholder to test a big wall of text in a row.",
+      ],
+    },
+    {
+      version: "HiGuard 0.0.075",
+      date: "August 4th, 2024",
+      changes: [
+        "Client SDK initialized. SKRRRRRRRRRR. Lorem Ipsum Testing to see the bullet points to see if they wrap correclty so I'm writing a quite mundane paragraph to act as a placeholder to test a big wall of text in a row., Testing to see the bullet points to see if they wrap correclty so I'm writing a quite mundane paragraph to act as a placeholder to test a big wall of text in a row., Testing to see the bullet points to see if they wrap correclty so I'm writing a quite mundane paragraph to act as a placeholder to test a big wall of text in a row., Testing to see the bullet points to see if they wrap correclty so I'm writing a quite mundane paragraph to act as a placeholder to test a big wall of text in a row.",
+      ],
+    },
+    {
+      version: "HiGuard 0.0.00008",
+      date: "August 1st, 2024",
+      changes: ["Initial alpha release notes..."],
+    },
+  ];
+
   return (
     <div className="bg-slate-50 text-slate-900 w-full flex flex-row relative dark:bg-slate-800 dark:text-slate-200">
       <div
@@ -13,20 +36,19 @@ const Releases = () => {
       >
         <HomeSidebar />
       </div>
+
       <div className="min-w-52" />
-      <Card>
-        <h1>Releases</h1>
-        <ul>
-          <li>
-            <h2>Version 1.0.0</h2>
-            <ol>
-              <li>Base backend services for namespace, users, errors</li>
-              <li>Base frontend skeleton complete for main services</li>
-              <li>Client SDK initialized</li>
-            </ol>
-          </li>
-        </ul>
-      </Card>
+      <div className="flex flex-col  px-10 items-left w-3/4">
+        <h1 className="font-bold text-6xl mb-8">HiGuard: Release Notes</h1>
+        {releaseData.map((log, index) => (
+          <ChangeLogCard
+            key={index}
+            version={log.version}
+            date={log.date}
+            changes={log.changes}
+          />
+        ))}
+      </div>
     </div>
   );
 };
