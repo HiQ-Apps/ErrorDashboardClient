c6f718f3dd2ff8728e4d150bb31c8fea9fc68df0
Daywick <derekli11204@gmail.com>
2024-08-19 05:24:37 -0400

    Updated card height

--- client/src/components/base/LoadingCard/LoadingCard.tsx
+++ client/src/components/base/LoadingCard/LoadingCard.tsx
@@ -2,7 +2,7 @@ import { BiLoaderCircle } from "react-icons/bi";
 
 const LoadingCard = () => {
   return (
-    <div className="w-full h-36 flex justify-center p-24">
+    <div className="w-full h-full flex justify-center p-24">
       <BiLoaderCircle className="w-24 h-24 animate-ease-in-out-rotation" />
     </div>
   );
